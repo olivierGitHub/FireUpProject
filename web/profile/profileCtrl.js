e385b5332f7b5cd52dfabffe2c8c83621f6aa569
OlivierGitHub <olivier_coulibaly@yahoo.fr>
2015-03-14 20:08:10 +0100

    Profile form
    Work in progress

--- web/profile/profileCtrl.js
+++ web/profile/profileCtrl.js
@@ -3,8 +3,32 @@
  */
 
 /* global angular */
-angular.module('profileApp', [])
+angular.module('profileApp', ['ui.router'])
 
-    .controller('profileController', ['$scope', function($scope){
+    .controller('profileController', ['$scope', 'customDatepicker', function($scope, customDatepicker){
         'use strict';
-    }]);
+        $scope.birthDate = '2013-07-23';
+        $scope.dateOptions = {};
+    }])
+    .directive('customDatepicker',function($compile,$timeout){
+        'use strict';
+        return {
+            replace:true,
+            templateUrl:'custom-datepicker.html',
+            scope: {
+                ngModel: '=',
+                dateOptions: '@',
+                dateDisabled: '@',
+                opened: '=',
+                min: '@',
+                max: '@',
+                popup: '@',
+                options: '@',
+                name: '@',
+                id: '@'
+            },
+            link: function($scope, $element, $attrs, $controller){
+
+            }
+        };
+    });
