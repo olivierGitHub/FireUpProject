9e3b282b582da6e250d4a6b2116a0ca465ff62ee
OlivierGitHub <olivier_coulibaly@yahoo.fr>
2015-03-21 01:18:38 +0100

    DAO + WebService for content page
    need test
    +
    need to add Read Update Delete and Find All web services

--- src/main/java/video/service/videoWebService.java
+++ src/main/java/video/service/videoWebService.java
@@ -1,7 +1,35 @@
 package video.service;
 
+import video.Video;
+import video.dataAccess.DaoVideo;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
 /**
  * Created by oliver on 20/03/15.
  */
+@Path("/video")
 public class VideoWebService {
+
+    @POST
+    @Path("/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    public int createVideo(
+                        @QueryParam("titleVideo") String title,
+                        @QueryParam("descriptionVideo") String description,
+                        @QueryParam("linkVideo") String link,
+                        @QueryParam("listTagsVideo") String listTags){
+        Video video = new Video();
+            video.setTitleVideo(title);
+            video.setDescriptionVideo(description);
+            video.setLinkVideo(link);
+            video.setListTagsVideo(listTags);
+        return DaoVideo.getInstance().create(video);
+    }
+
 }
