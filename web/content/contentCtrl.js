e4f322ac11438b61fd942ef8e290e630ca10541d
OlivierGitHub <olivier_coulibaly@yahoo.fr>
2015-03-28 04:31:53 +0100

    Control on input field of Content page
    DONE using ng-class (modify css class and ng-keyup for the event listener)
    
    To Do - DAO + WebService for content page:
    Erase values from input fields once inserted in database (coded but don't work)
    need to add Read Update Delete and Find All web services

--- web/content/contentCtrl.js
+++ web/content/contentCtrl.js
@@ -11,34 +11,174 @@ angular.module('contentApp',[])
     .controller('contentController', ['$scope', 'contentService', function($scope, contentService){
         'use strict';
 
-        $scope.errorClass = "contentInput inputStyleContent";
+        $scope.classTitleVideo = "contentInput inputStyleContent";
+        $scope.classDescriptionVideo = "contentInput inputStyleContent";
+        $scope.classLinkVideo = "contentInput inputStyleContent";
+        $scope.classListTagsVideo = "contentInput inputStyleContent";
+        $scope.classSubmitVideo = "submitContentError";
+        $scope.isFormValidVideo = false;
+
+        $scope.checkFormVideo = function(){
+            if ($scope.titleVideo.length < 30 && $scope.titleVideo !== undefined &&
+                $scope.descriptionVideo.length < 20 && $scope.descriptionVideo !== undefined &&
+                $scope.linkVideo.length < 50 && $scope.linkVideo !== undefined &&
+                $scope.listTagsVideo.length < 100 && $scope.listTagsVideo !== undefined){
+                    $scope.classSubmitVideo = "submitContent";
+                    $scope.isFormValidVideo = true;
+            }else{
+                $scope.isFormValidVideo = false;
+                $scope.classSubmitVideo = "submitContentError";
+            }
+        };
 
         $scope.checkLengthTitleVideo = function(){
             if ($scope.titleVideo.length < 30){
-                $scope.errorClass = "contentInput inputStyleContent";
+                $scope.classTitleVideo = "contentInput inputStyleContent";
             }else{
-                $scope.errorClass = "contentInput inputStyleContentError";
+                $scope.classTitleVideo = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthDescriptionVideo = function(){
+            if ($scope.descriptionVideo.length < 20){
+                $scope.classDescriptionVideo = "contentInput inputStyleContent";
+            }else{
+                $scope.classDescriptionVideo = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthLinkVideo = function(){
+            if ($scope.linkVideo.length < 50){
+                $scope.classLinkVideo = "contentInput inputStyleContent";
+            }else{
+                $scope.classLinkVideo = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthListTagsVideo = function(){
+            if ($scope.listTagsVideo.length < 100){
+                $scope.classListTagsVideo = "contentInput inputStyleContent";
+            }else{
+                $scope.classListTagsVideo = "contentInput inputStyleContentError";
             }
         };
 
         $scope.validateVideo = function(){
-            if ($scope.titleVideo.length < 50 && $scope.titleVideo !== undefined &&
-                $scope.descriptionVideo.length < 50 && $scope.descriptionVideo !== undefined &&
-                $scope.linkVideo.length < 50 && $scope.linkVideo !== undefined &&
-                $scope.listTagsVideo.length < 50 && $scope.listTagsVideo !== undefined){
-                    contentService.createVideo($scope.titleVideo,$scope.descriptionVideo,$scope.linkVideo,$scope.listTagsVideo);
-                    contentService.eraseInputValues($scope.titleVideo,$scope.descriptionVideo,$scope.linkVideo,$scope.listTagsVideo);
-            }else{
-                    console.log("check data you've just entered");
-            }
+            contentService.createVideo($scope.titleVideo,$scope.descriptionVideo,$scope.linkVideo,$scope.listTagsVideo);
+            contentService.eraseInputValues($scope.titleVideo,$scope.descriptionVideo,$scope.linkVideo,$scope.listTagsVideo);
         };
 
 
+        $scope.classTitleWebsite = "contentInput inputStyleContent";
+        $scope.classDescriptionWebsite = "contentInput inputStyleContent";
+        $scope.classLinkWebsite = "contentInput inputStyleContent";
+        $scope.classListTagsWebsite = "contentInput inputStyleContent";
+        $scope.classSubmitWebsite = "submitContentError";
+        $scope.isFormValidWebsite = false;
+
+        $scope.checkFormWebsite = function(){
+            if ($scope.titleWebsite.length < 30 && $scope.titleWebsite !== undefined &&
+                $scope.descriptionWebsite.length < 20 && $scope.descriptionWebsite !== undefined &&
+                $scope.linkWebsite.length < 50 && $scope.linkWebsite !== undefined &&
+                $scope.listTagsWebsite.length < 100 && $scope.listTagsWebsite !== undefined){
+                $scope.classSubmitWebsite = "submitContent";
+                $scope.isFormValidWebsite = true;
+            }else{
+                $scope.isFormValidWebsite = false;
+                $scope.classSubmitWebsite = "submitContentError";
+            }
+        };
+
+        $scope.checkLengthTitleWebsite = function(){
+            if ($scope.titleWebsite.length < 30){
+                $scope.classTitleWebsite = "contentInput inputStyleContent";
+            }else{
+                $scope.classTitleWebsite = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthDescriptionWebsite = function(){
+            if ($scope.descriptionWebsite.length < 20){
+                $scope.classDescriptionWebsite = "contentInput inputStyleContent";
+            }else{
+                $scope.classDescriptionWebsite = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthLinkWebsite = function(){
+            if ($scope.linkWebsite.length < 50){
+                $scope.classLinkWebsite = "contentInput inputStyleContent";
+            }else{
+                $scope.classLinkWebsite = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthListTagsWebsite = function(){
+            if ($scope.listTagsWebsite.length < 100){
+                $scope.classListTagsWebsite = "contentInput inputStyleContent";
+            }else{
+                $scope.classListTagsWebsite = "contentInput inputStyleContentError";
+            }
+        };
 
         $scope.validateWebsite = function(){
             contentService.createWebsite($scope.titleWebsite,$scope.descriptionWebsite,$scope.linkWebsite,$scope.listTagsWebsite);
             contentService.eraseInputValues($scope.titleWebsite,$scope.descriptionWebsite,$scope.linkWebsite,$scope.listTagsWebsite);
         };
+
+
+        $scope.classTitleSocialNetwork = "contentInput inputStyleContent";
+        $scope.classDescriptionSocialNetwork = "contentInput inputStyleContent";
+        $scope.classLinkSocialNetwork = "contentInput inputStyleContent";
+        $scope.classListTagsSocialNetwork = "contentInput inputStyleContent";
+        $scope.classSubmitSocialNetwork = "submitContentError";
+        $scope.isFormValidSocialNetwork = false;
+
+        $scope.checkFormSocialNetwork = function(){
+            if ($scope.titleSocialNetwork.length < 30 && $scope.titleSocialNetwork !== undefined &&
+                $scope.descriptionSocialNetwork.length < 20 && $scope.descriptionSocialNetwork !== undefined &&
+                $scope.linkSocialNetwork.length < 50 && $scope.linkSocialNetwork !== undefined &&
+                $scope.listTagsSocialNetwork.length < 100 && $scope.listTagsSocialNetwork !== undefined){
+                $scope.classSubmitSocialNetwork = "submitContent";
+                $scope.isFormValidSocialNetwork = true;
+            }else{
+                $scope.isFormValidSocialNetwork = false;
+                $scope.classSubmitSocialNetwork = "submitContentError";
+            }
+        };
+
+        $scope.checkLengthTitleSocialNetwork = function(){
+            if ($scope.titleSocialNetwork.length < 30){
+                $scope.classTitleSocialNetwork = "contentInput inputStyleContent";
+            }else{
+                $scope.classTitleSocialNetwork = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthDescriptionSocialNetwork = function(){
+            if ($scope.descriptionSocialNetwork.length < 20){
+                $scope.classDescriptionSocialNetwork = "contentInput inputStyleContent";
+            }else{
+                $scope.classDescriptionSocialNetwork = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthLinkSocialNetwork = function(){
+            if ($scope.linkSocialNetwork.length < 50){
+                $scope.classLinkSocialNetwork = "contentInput inputStyleContent";
+            }else{
+                $scope.classLinkSocialNetwork = "contentInput inputStyleContentError";
+            }
+        };
+
+        $scope.checkLengthListTagsSocialNetwork = function(){
+            if ($scope.listTagsSocialNetwork.length < 100){
+                $scope.classListTagsSocialNetwork = "contentInput inputStyleContent";
+            }else{
+                $scope.classListTagsSocialNetwork = "contentInput inputStyleContentError";
+            }
+        };
+
         $scope.validateSocialNetwork = function(){
             contentService.createSocialNetwork($scope.titleSocialNetwork,$scope.descriptionSocialNetwork,$scope.linkSocialNetwork,$scope.listTagsSocialNetwork);
             contentService.eraseInputValues($scope.titleSocialNetwork,$scope.descriptionSocialNetwork,$scope.linkSocialNetwork,$scope.listTagsSocialNetwork);
